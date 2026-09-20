b1fd6f3a12fe56fbd0f61d53d9ecb075bf067b67
Nandan Nayak <nandannayak2102@gmail.com>
2026-09-20 11:38:23 +0530

    My first table :)

---
+++ SQL/test.sql
@@ -0,0 +1,14 @@
+-- Beginning-- 
+create database college;
+use college;
+create table student(
+rollno int,
+name varchar(30),
+age int
+);
+
+insert into student values
+(101,"adam",12),
+(102,"bob",14);
+
+select * from student;
