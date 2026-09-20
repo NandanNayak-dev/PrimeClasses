98d9e08218410c11275b5c51b59cc68230671f42
Nandan Nayak <nandannayak2102@gmail.com>
2026-09-20 15:44:13 +0530

    Basics

--- SQL/test.sql
+++ SQL/test.sql
@@ -31,6 +31,14 @@ followers int,
 constraint check (age>=13),
 primary key (id)
 );
+INSERT INTO user (id, age, name, email, followers)
+VALUES
+(1, 20, 'Nandan', 'nandan@gmail.com', 500),
+(2, 21, 'Rohan', 'rohan@gmail.com', 300),
+(3, 19, 'Nikhitha', 'nikhitha@gmail.com', 450),
+(4, 22, 'Neha', 'neha@gmail.com', 700),
+(5, 25, 'Rahul', 'rahul@gmail.com', 1000);
+ select * from user where followers>300;
 -- primary and foreign key concept
 create table post(
 id int primary key,
