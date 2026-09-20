419abcd3f200ea9f7b8ced26163d063d234502cf
Nandan Nayak <nandannayak2102@gmail.com>
2026-09-20 13:49:06 +0530

    PrimaryKey ForeignKey

--- SQL/test.sql
+++ SQL/test.sql
@@ -18,3 +18,25 @@ create database if not exists instagram;
 drop database if exists instagram;
 show databases;
 show tables;
+
+-- table queries--
+ 
+-- drop table user;
+create table user(
+id int,
+age int,
+name varchar(30),
+email varchar(50),
+followers int,
+constraint check (age>=13),
+primary key (id)
+);
+-- primary and foreign key concept
+create table post(
+id int primary key,
+content varchar(30),
+user_id int,
+foreign key (user_id)  references user(id)
+);
+
+
